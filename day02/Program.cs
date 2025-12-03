3eb3b950097b0535d7e0afcc6c88e915c38ce080
Björn Weström <bjorn.westrom@cag.se>
2025-12-03 16:53:30 +0100

    Day 02 and Day 03

--- day02/Program.cs
+++ day02/Program.cs
@@ -2,34 +2,147 @@
 string ProjectDir() => System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetCallingAssembly().Location)!;
 string FilePath(string file) => Path.Join(ProjectDir(), "../../../", file);
 
+bool ValidId(long id)
+{
+    if (id > 9 && id <= 99) return (id / 10 != id % 10);
+    if (id > 999 && id <= 9999) return (id / 100 != id % 100);
+    if (id > 99999 && id <= 999999) return (id / 1000 != id % 1000);
+    if (id > 9999999 && id <= 99999999) return (id / 10000 != id % 10000);
+    if (id > 999999999 && id <= 9999999999) return (id / 100000 != id % 100000);
+    return true;
+}
+
 void SolveOne(string file)
 {
     Console.WriteLine($"{Project()}.1 Solve for: " + file);
     var sw = System.Diagnostics.Stopwatch.StartNew();
 
-    var rotations = File.ReadAllLines(FilePath(file));
+    var ranges = File.ReadAllText(FilePath(file)).Split(",").ToList();
+    long invalidIdSum = 0;
 
-    sw.Stop();   
-    //Console.WriteLine("Times at zero: " + timesAtZero);
+    foreach (var range in ranges)
+    {
+        var start = long.Parse(range.Split("-")[0]);
+        var end = long.Parse(range.Split("-")[1]);
+        for (long i = start; i <= end; i++)
+        {
+            if (!ValidId(i))
+            {
+                //Console.WriteLine("Invalid " + i);
+                invalidIdSum += i;
+            }
+        }
+    }
+
+    sw.Stop();
+    Console.WriteLine("Invalid id sum: " + invalidIdSum);
     Console.WriteLine("Runtime: " + sw.ElapsedMilliseconds.ToString("0.000") + " ms");
     Console.WriteLine("");
 }
 
 SolveOne("ex.txt");
-//SolveOne("in.txt");
+SolveOne("in.txt");
+
+
+bool ValidIdTwo(long id)
+{
+    if (id < 10) return true;
+
+    //if (id > 9 && id <= 99) return (id / 10 != id % 10);
+    //if (id > 999 && id <= 9999) return (id / 100 != id % 100);
+    //if (id > 99999 && id <= 999999) return (id / 1000 != id % 1000);
+    //if (id > 9999999 && id <= 99999999) return (id / 10000 != id % 10000);
+    //if (id > 999999999 && id <= 9999999999) return (id / 100000 != id % 100000);
+
+    var str = id.ToString();
+    if (str.All(c => c == str[0])) return false;
+
+    if (str.Length > 2 && str.Length % 2 == 0)
+    {
+        bool all = true;
+        var match = str.Substring(0, 2);
+        for (int i = 2; all && i < str.Length; i+= 2)
+        {
+            if (match != str.Substring(i,2)) all = false;
+        }
+        if (all) return false;
+    }
+
+    if (str.Length > 3 && str.Length % 3 == 0)
+    {
+        bool all = true;
+        var match = str.Substring(0, 3);
+        for (int i = 3; all && i < str.Length; i += 3)
+        {
+            if (match != str.Substring(i, 3)) all = false;
+        }
+        if (all) return false;
+    }
+
+    if (str.Length > 4 && str.Length % 4 == 0)
+    {
+        bool all = true;
+        var match = str.Substring(0, 4);
+        for (int i = 4; all && i < str.Length; i += 4)
+        {
+            if (match != str.Substring(i, 4)) all = false;
+        }
+        if (all) return false;
+    }
+
+    if (str.Length > 5 && str.Length % 5 == 0)
+    {
+        bool all = true;
+        var match = str.Substring(0, 5);
+        for (int i = 5; all && i < str.Length; i += 5)
+        {
+            if (match != str.Substring(i, 5)) all = false;
+        }
+        if (all) return false;
+    }
+
+    if (str.Length > 6 && str.Length % 6 == 0)
+    {
+        bool all = true;
+        var match = str.Substring(0, 6);
+        for (int i = 6; all && i < str.Length; i += 6)
+        {
+            if (match != str.Substring(i, 6)) all = false;
+        }
+        if (all) return false;
+    }
+
+    
+    return true;
+}
 
 void SolveTwo(string file)
 {
     Console.WriteLine($"{Project()}.2 Solve for: " + file);
     var sw = System.Diagnostics.Stopwatch.StartNew();
 
-    var rotations = File.ReadAllLines(FilePath(file));
+    var ranges = File.ReadAllText(FilePath(file)).Split(",").ToList();
+    long invalidIdSum = 0;
+
+    foreach (var range in ranges)
+    {
+        var start = long.Parse(range.Split("-")[0]);
+        var end = long.Parse(range.Split("-")[1]);
+        for (long i = start; i <= end; i++)
+        {
+            if (!ValidIdTwo(i))
+            {
+                //Console.WriteLine("Invalid " + i);
+                invalidIdSum += i;
+            }
+        }
+    }
 
     sw.Stop();
-    //Console.WriteLine("Times at zero: " + timesAtZero);
+    Console.WriteLine("Invalid id sum: " + invalidIdSum);
     Console.WriteLine("Runtime: " + sw.ElapsedMilliseconds.ToString("0.000") + " ms");
     Console.WriteLine("");
 }
 
-//SolveTwo("ex.txt");
-//SolveTwo("in.txt");
+SolveTwo("ex.txt");
+SolveTwo("in.txt");
---
+++ day03/Program.cs
@@ -0,0 +1,64 @@
+﻿string Project() => System.Reflection.Assembly.GetCallingAssembly().GetName().Name!;
+string ProjectDir() => System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetCallingAssembly().Location)!;
+string FilePath(string file) => Path.Join(ProjectDir(), "../../../", file);
+
+void SolveOne(string file)
+{
+    Console.WriteLine($"{Project()}.1 Solve for: " + file);
+    var sw = System.Diagnostics.Stopwatch.StartNew();
+
+    var banks = File.ReadAllLines(FilePath(file));
+    long sum = 0;
+
+    foreach (var bank in banks)
+    {
+        var bankPos = bank.ToList();
+        var maxFirstDigit = bankPos.Take(bank.Length - 1).Max();
+        var firstDigitPos = bankPos.Take(bank.Length - 1).ToList().IndexOf(maxFirstDigit);
+        var maxLastDigit = bankPos.Skip(firstDigitPos + 1).Max();
+        var joules = (maxFirstDigit - '0') * 10 + (maxLastDigit - '0');
+        sum += joules;
+    }
+
+    sw.Stop();
+    Console.WriteLine("Sum: " + sum);
+    Console.WriteLine("Runtime: " + sw.ElapsedMilliseconds.ToString("0.000") + " ms");
+    Console.WriteLine("");
+}
+
+SolveOne("ex.txt");
+SolveOne("in.txt");
+
+
+void SolveTwo(string file)
+{
+    Console.WriteLine($"{Project()}.2 Solve for: " + file);
+    var sw = System.Diagnostics.Stopwatch.StartNew();
+
+    var banks = File.ReadAllLines(FilePath(file));
+    long sum = 0;
+
+    foreach (var bank in banks)
+    {
+        var bankPos = bank.ToList();
+        long joulets = 0;
+        var lastDigitPos = -1;
+        for (int i = 11; i >= 0; i--)
+        {
+            var searchSpace = bankPos.Take(bank.Length - i).Skip(lastDigitPos + 1);
+            var maxDigit = searchSpace.Max();
+            lastDigitPos = (searchSpace.ToList().IndexOf(maxDigit) + lastDigitPos + 1);
+            joulets += (maxDigit - '0') * (long)Math.Pow(10, i);
+        }
+        //Console.WriteLine(joulets);
+        sum += joulets;
+    }
+
+    sw.Stop();
+    Console.WriteLine("Sum: " + sum);
+    Console.WriteLine("Runtime: " + sw.ElapsedMilliseconds.ToString("0.000") + " ms");
+    Console.WriteLine("");
+}
+
+SolveTwo("ex.txt"); // Expect 3121910778619
+SolveTwo("in.txt");
